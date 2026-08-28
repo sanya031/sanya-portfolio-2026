3cc9bdc4e20df586b9971b3b12bc1b83c4a79128
Sanya Malhotra <69260951+sanya031@users.noreply.github.com>
2026-08-28 12:00:35 -0400

    changes made to the interactive statementn

--- components/home/HomePage.tsx
+++ components/home/HomePage.tsx
@@ -20,7 +20,7 @@ export function HomePage() {
 
       <HeroCrossfadeShell>
         <HeroIntro />
-        <ScrollStatement words={statementWords} />
+       {/* <ScrollStatement words={statementWords} /> */}
       </HeroCrossfadeShell>
 
       <WorkSection caseStudies={caseStudies} />
